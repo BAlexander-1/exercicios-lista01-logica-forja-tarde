975b0c2db3be0c3ba6783f24ae45790b23a789c4
locd <4bryanby@gmail.com>
2025-10-20 13:51:37 -0300

    Delete EXER2 directory

--- EXER2/script.js
+++
@@ -1,9 +0,0 @@
-function escreva() {
-    var idade = parseFloat(document.getElementById("idade").value)
-
-    if (idade < 18) {
-        printIdade.textContent = "Voce e menor de idade"
-    } else {
-        printIdade.textContent = "Voce e maior de idade"
-    }
-}
